a8f8584f7625689902f7376b58fc3d2ab13cc45a
aweihmayer <a.weihmayer@gmail.com>
2026-04-19 14:23:17 -0400

    update

--- save.py
+++ save.py
@@ -1,24 +1,3 @@
 from src import *
 
-t = Article(
-    id='triangle',
-    name='Triangle',
-    name_suffix_1=None,
-    name_suffix_2=None,
-    other_names=None,
-    summary='TODO',
-    types=[ArticleType.SUBMISSION_CHOKEHOLD],
-    difficulty=ArticleDifficulty.UNIVERSAL,
-    requires_gi=False,
-    ranking=0,
-    parent=None,
-    inverse=None,
-    followups=[],
-    counters=[],
-    concepts=[],
-    content=[]
-)
-
-#create(t)
-# update(t)
 clean()
